595e56de843bfd24f00a27348bdb9564dde7efd0
abduredwan90@gmail.com <smart@kali.kali>
2024-01-21 01:26:13 +0300

    HelloCR7

--- viteis/src/SearchBox.jsx
+++ viteis/src/SearchBox.jsx
@@ -1,10 +1,11 @@
-const SearchBox = () => {
+const SearchBox = ({ SearchChange }) => {
 	return (
 		<div className="Search">
 			<input
 				className="SearchBox"
 				type="Search"
 				placeholder="Search Robots"
+				onChange={SearchChange}
 			/>
 		</div>
 	);
