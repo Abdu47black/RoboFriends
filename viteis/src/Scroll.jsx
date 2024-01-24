0577b4157ed023492cd0a32a7ffc520cae4110fa
Abdu47black <abduredwan90@gmail.com>
2024-01-24 18:36:04 +0300

    Filtering has been done

---
+++ viteis/src/Scroll.jsx
@@ -0,0 +1,14 @@
+const Scroll = (props) => {
+	return (
+		<div
+			style={{
+				overflowY: "scroll",
+				border: "1px solid orange",
+				height: "500px",
+			}}
+		>
+			{props.children}
+		</div>
+	);
+};
+export default Scroll;
--- viteis/src/card.jsx
+++ viteis/src/card.jsx
@@ -3,7 +3,7 @@ import "/src/card.css";
 export default function Abdul({ name, email, id }) {
   return (
     <div className="start">
-      <img alt="robots" src={`https://robohash.org/${id}?200*200`} />
+      <img alt="robots" src={`https://robohash.org/${id}?`} />
       <div>
         <h1>{name}</h1>
         <p> {email}</p>
