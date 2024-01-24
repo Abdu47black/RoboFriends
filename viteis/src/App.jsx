b88ffd31b177dc206799241b40f68a162dde05c8
abduredwan90@gmail.com <smart@kali.kali>
2024-01-24 23:08:42 +0300

    i was force

--- viteis/src/App.jsx
+++ viteis/src/App.jsx
@@ -5,30 +5,30 @@ import { Component } from "react";
 import "/src/App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: robots,
-      SearchField: "",
-    };
-  }
-  onSearchChange = (event) => {
-    this.setState({ SearchField: event.target.value });
-  };
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			SearchField: "",
+		};
+	}
+	onSearchChange = (event) => {
+		this.setState({ SearchField: event.target.value });
+	};
 
-  render() {
-    const FilterdRobots = this.state.robots.filter((robots) => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.SearchField.toLowerCase());
-    });
-    return (
-      <div className="Title">
-        <h1 id="one">RoboFriends</h1>
-        <SearchBox SearchChange={this.onSearchChange} />
-        <Cardlist robots={FilterdRobots} />
-      </div>
-    );
-  }
+	render() {
+		const FilterdRobots = this.state.robots.filter((robots) => {
+			return robots.name
+				.toLowerCase()
+				.includes(this.state.SearchField.toLowerCase());
+		});
+		return (
+			<div className="Title">
+				<h1 id="one">RoboFriends</h1>
+				<SearchBox SearchChange={this.onSearchChange} />
+				<Cardlist robots={FilterdRobots} />
+			</div>
+		);
+	}
 }
 export default App;
--- viteis/src/Cardlist.jsx
+++ viteis/src/Cardlist.jsx
@@ -2,7 +2,7 @@ import Abdul from "./card";
 import { robots } from "/src/robots.js";
 import "/src/card.css";
 
-export default function Cardlist() {
+export default function Cardlist({ robots }) {
   return (
     <div>
       {robots.map((user, i) => {
